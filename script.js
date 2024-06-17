4679167726633276fddabdc53b44326ce47ae4f1
LwisH14 <168606155+LwisH14@users.noreply.github.com>
2024-06-17 12:52:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('file-input').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('profile-pic').src = e.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
